970fa5c642f580e5be2c001bc24acf2a0248ee68
xiaobaige <zitongbai@outlook.com>
2026-02-08 22:54:09 +0800

    Add vanilla RL

---
+++ source/legged_lab/legged_lab/tasks/locomotion/velocity/config/g1/__init__.py
@@ -0,0 +1,28 @@
+import gymnasium as gym
+
+from . import agents
+
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="LeggedLab-Isaac-Velocity-Flat-Unitree-G1-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.flat_env_cfg:G1FlatEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:G1FlatPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="LeggedLab-Isaac-Velocity-Flat-Unitree-G1-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.flat_env_cfg:G1FlatEnvCfg_PLAY",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:G1FlatPPORunnerCfg",
+    },
+)
